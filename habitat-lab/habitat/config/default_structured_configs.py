ff081ee98090fcfb90e4e8d10fe650098730bfcc
Tiffany Min <soyeonm@devfair0470.h2.fair>
2023-11-14 14:49:48 -0800

    hbao true

--- habitat-lab/habitat/config/default_structured_configs.py
+++ habitat-lab/habitat/config/default_structured_configs.py
@@ -1483,6 +1483,7 @@ class HabitatSimV0Config(HabitatBaseConfig):
     allow_sliding: bool = True
     frustum_culling: bool = True
     enable_physics: bool = False
+    enable_hbao: bool = True
     physics_config_file: str = "./data/default.physics_config.json"
     # Possibly unstable optimization for extra performance
     # with concurrent rendering
